13ec0e916e9372372fcc70136c1511fb436f3052
yangsungbin77 <yangsungbin77@gmail.com>
2026-06-17 12:26:55 +0900

    구글 서치 콘솔 인증 메타태그 추가

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = '게임월드7';
 export const SITE_DESCRIPTION = '게임 뉴스 · 공략 · 리뷰를 가장 빠르게 전하는 게임 블로그';
+
+// 구글 서치 콘솔 'HTML 태그' 인증의 content 값만 넣는다. 비워두면 메타태그가 출력되지 않는다.
+export const GSC_VERIFICATION = '1I9RLg6bwsE0GgOuulefYoYIOUPmDYV4V18ekR4iwuE';
